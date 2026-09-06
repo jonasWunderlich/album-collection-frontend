44000af457a96ed06bbfee58c729f3630f9e9b74
Jonas Wunderlich <jonas@systemkultur.de>
2026-09-06 09:14:55 +0200

    link to google image search if urlCover returns error

--- src/app/components/album-tile/album-tile.ts
+++ src/app/components/album-tile/album-tile.ts
@@ -11,4 +11,25 @@ import { Album } from '../../api/model/album';
 export class AlbumTile {
   @Input() album!: Album;
   @Input() lazy = true;
+
+  imageError = false;
+
+  onImageError(): void {
+    this.imageError = true;
+  }
+
+  hasValidCover(): boolean {
+    return (
+      !!this.album?.urlCover && this.album.urlCover.trim() !== '' && !this.imageError
+    );
+  }
+
+  googleImageSearchUrl(value: Album) {
+    if (!value) return '#';
+
+    const artists = value.albumArtist?.map(a => a.trim()).join(', ') ?? '';
+    const searchQuery = `${artists} - ${value.title}`;
+
+    return `https://www.google.com/search?tbm=isch&q=${encodeURIComponent(searchQuery)}`;
+  }
 }
